8801bf2996b5d53d658ee803a98bd1e8a64a3c57
Zirk <xwilarg@yahoo.fr>
2017-12-17 22:20:58 +0100

    Adding teleportation to others rooms

---
+++ Assets/Scripts/teleport.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class teleport : MonoBehaviour {
+
+    public GameObject newRoom;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (newRoom != null && other.name == "Camera (eye)")
+            GameObject.Find("[CameraRig]").transform.position = newRoom.transform.position;
+    }
+}
