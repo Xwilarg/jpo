bc221a6a0ccf50fdf66d684d37e8fae1a2b18df7
Zirk <xwilarg@yahoo.fr>
2017-12-17 23:04:13 +0100

    Can now launch objects

---
+++ Assets/Scripts/ControllerGrab.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerGrab : MonoBehaviour {
+
+    private SteamVR_TrackedController controller;
+
+    private List<GameObject> grappable;
+    private GameObject currGrab;
+    private Vector3 lastPos;
+
+    private void GrabObject(object sender, ClickedEventArgs e)
+    {
+        if (grappable.Count > 0)
+        {
+            currGrab = grappable[0];
+            Rigidbody rb = currGrab.GetComponent<Rigidbody>();
+            currGrab.transform.parent = transform;
+            rb.isKinematic = true;
+            rb.useGravity = false;
+            rb.velocity = Vector3.zero;
+        }
+    }
+
+    private void DropObject(object sender, ClickedEventArgs e)
+    {
+        if (currGrab != null)
+        {
+            Rigidbody rb = currGrab.GetComponent<Rigidbody>();
+            rb.velocity = (transform.position - lastPos) * 10f;
+            drop();
+        }
+    }
+
+    public void drop()
+    {
+        Rigidbody rb = currGrab.GetComponent<Rigidbody>();
+        rb.useGravity = true;
+        rb.isKinematic = false;
+        currGrab.transform.parent = null;
+        currGrab = null;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Grappable"))
+            grappable.Add(other.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Grappable"))
+            grappable.Remove(other.gameObject);
+    }
+
+    private void Start ()
+    {
+        currGrab = null;
+        lastPos = Vector3.zero;
+        grappable = new List<GameObject>();
+        controller = GetComponent<SteamVR_TrackedController>();
+        controller.TriggerClicked += GrabObject;
+        controller.TriggerUnclicked += DropObject;
+	}
+
+
+    public void Update()
+    {
+        if (currGrab != null)
+        {
+            lastPos = transform.position;
+        }
+    }
+}
--- Assets/Scripts/teleport.cs
+++ Assets/Scripts/teleport.cs
@@ -1,12 +1,23 @@
 ﻿using UnityEngine;
 
-public class teleport : MonoBehaviour {
-
+public class teleport : MonoBehaviour
+{
     public GameObject newRoom;
 
     private void OnTriggerEnter(Collider other)
     {
         if (newRoom != null && other.name == "Camera (eye)")
             GameObject.Find("[CameraRig]").transform.position = newRoom.transform.position;
+        else
+        {
+            other.transform.position = newRoom.transform.position;
+
+            GameObject controllerL = GameObject.Find("Controller (left)");
+            GameObject controllerR = GameObject.Find("Controller (right)");
+            if (controllerL != null)
+                controllerL.GetComponent<ControllerGrab>().drop();
+            if (controllerR != null)
+                controllerR.GetComponent<ControllerGrab>().drop();
+        }
     }
 }
