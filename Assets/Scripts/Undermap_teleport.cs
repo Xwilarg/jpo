6537585e89923115bc76025ff76668af8328d676
MrAchymist <paul.gaston@sfr.fr>
2017-12-22 14:44:48 +0100

    Objects now teleport when going undermap

---
+++ Assets/Scripts/Undermap_teleport.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Undermap_teleport : MonoBehaviour
+{
+    private GameObject Totel;
+    private Vector3 TotelPos;
+    private void OnCollisionEnter(Collision other)
+    {
+        Totel = other.gameObject;
+        TotelPos = Totel.transform.position;
+        TotelPos.y = 1.5f;
+        Totel.transform.position = TotelPos;
+    }
+}
