7da7c66839d160127a6777fa1a6244e9d6cf9514
Zirk <xwilarg@yahoo.fr>
2018-02-09 12:05:04 +0100

    Randomly move hexapod paws

--- Assets/Scripts/HexapodPaw.cs
+++ Assets/Scripts/HexapodPaw.cs
@@ -1,16 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class HexapodPaw : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Update()
+    {
+        transform.Rotate(new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)));
+    }
 }
