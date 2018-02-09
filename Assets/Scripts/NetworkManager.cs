0543f7cd67983620b2af9a143dce43268c97d33b
Xwilarg <xwilarg@yahoo.fr>
2018-02-09 23:56:58 +0100

    Add basic networking interaction with Kiya

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+
+    private TcpListener server;
+    private TcpClient client = new TcpClient();
+
+    private void Start()
+    {
+        const int port = 7777;
+        server = new TcpListener(IPAddress.Any, port);
+        server.Start();
+        StartListening();
+    }
+
+    public void sendDatas(string data)
+    {
+        NetworkStream s = client.GetStream();
+        StreamWriter sw = new StreamWriter(s);
+        sw.WriteLine("JPO" + data);
+        sw.Flush();
+    }
+
+    private void Update()
+    {
+        try
+        {
+            NetworkStream s = client.GetStream();
+            StreamReader sr = new StreamReader(s, true);
+            if (s.DataAvailable)
+            {
+                string datas = sr.ReadLine();
+                if (datas.Length > 3 && datas.Substring(0, 3) == "JPO")
+                {
+                    datas = datas.Substring(3, datas.Length - 3);
+                    if (datas == "PING")
+                        sendDatas("PING");
+                    else if (datas == "PINGREG")
+                        sendDatas("PINGREG");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError(ex.Message);
+        }
+    }
+
+    private void StartListening()
+    {
+        server.BeginAcceptTcpClient(AcceptTcpClient, server);
+    }
+
+    private void AcceptTcpClient(IAsyncResult iar)
+    {
+        TcpListener listener = (TcpListener)iar.AsyncState;
+        client = listener.EndAcceptTcpClient(iar);
+        StartListening();
+    }
+}
