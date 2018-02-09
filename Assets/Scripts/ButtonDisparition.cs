f408b1d00e1396a747047ea04e7688f33c53f2ca
MrAchymist <paul.gaston@sfr.fr>
2018-02-09 10:52:46 +0100

    Buttons enter inside the floor

---
+++ Assets/Scripts/ButtonDisparition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonDisparition : MonoBehaviour {
+    public float speed;
+    private Transform dest;
+    private Vector3 velocity;
+    private Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+        velocity = Vector3.zero;
+        transform.position = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        position = dest.TransformPoint(new Vector3(0, 5, -10));
+        transform.position = Vector3.SmoothDamp(transform.position, position, ref velocity, speed);
+	}
+}
